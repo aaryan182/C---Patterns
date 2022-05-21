721e84f77b3d594ee261a8464eedded0825785bc
Aaryan Bajaj <aaryanbajaj385@gmail.com>
2022-05-21 18:50:49 +0530

    added min part

--- ARRAYS/maxmin.cpp
+++ ARRAYS/maxmin.cpp
@@ -11,10 +11,23 @@ int getMax(int arr[], int n)
     return maxi;
 }
 
+int getMin(int arr[], int n)
+{
+    int mini = INT_MAX;
+    for (int i = 0; i < n; i++)
+    {
+        mini = min(mini, arr[i]);
+    }
+    return mini;
+}
+
 int main()
 {
     int arr[] = {3, 4, 9, 6};
     getMax(arr, 4);
     cout << "Maximum value is :" << getMax(arr, 4) << endl;
+    getMin(arr, 4);
+    cout << "Minimum value is :" << getMin(arr, 4) << endl;
+
     return 0;
 }
